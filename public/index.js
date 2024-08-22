c1c640cd7dde1511d4c76e1942a582aae565080a
Zvezda Neycheva <zvezda.neycheva.a55@learn.telerikacademy.com>
2024-08-22 19:35:04 +0300

    change: promise to async / await

--- public/index.js
+++ public/index.js
@@ -1,22 +1,36 @@
- // Fetch images from server and display them in the gallery
- fetch('/api/images')
- .then(response => response.json())
- .then(data => {
-   const gallery = document.getElementById('gallery');
-   data.images.forEach(file => {
-     const div = document.createElement('div');
-     div.classList.add('col-md-3');
-     div.innerHTML = `
-       <div class="card">
-         <img src="/uploads/${file}" class="card-img-top" alt="...">
-         <div class="card-body text-center">
-           <form action="/delete" method="POST">
-             <input type="hidden" name="filename" value="${file}">
-             <button type="submit" class="btn btn-danger">Delete</button>
-           </form>
-         </div>
-       </div>
-     `;
-     gallery.appendChild(div);
-   });
- });
+async function fetchAndDisplayImages() {
+  try {
+    const response = await fetch('/api/images');
+    
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    
+    const data = await response.json();
+    
+    const gallery = document.getElementById('gallery');
+    
+    gallery.innerHTML = '';
+    
+    data.images.forEach(file => {
+      const div = document.createElement('div');
+      div.classList.add('col-md-3');
+      div.innerHTML = `
+        <div class="card">
+          <img src="/uploads/${file}" class="card-img-top" alt="Image">
+          <div class="card-body text-center">
+            <form action="/delete" method="POST">
+              <input type="hidden" name="filename" value="${file}">
+              <button type="submit" class="btn btn-danger">Delete</button>
+            </form>
+          </div>
+        </div>
+      `;
+      gallery.appendChild(div);
+    });
+  } catch (error) {
+    console.error('There was a problem with the fetch operation:', error);
+  }
+}
+
+fetchAndDisplayImages();
